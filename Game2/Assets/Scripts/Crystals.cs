ef69c377682bcd239c425770887e221af5abcb7f
unknown <RobertoZV15>
2018-08-08 10:06:29 -0600

    version 1

---
+++ Game2/Assets/Scripts/Crystals.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Crystals : MonoBehaviour
+{
+
+    private Controls player;
+    public AudioSource bling;
+
+    void Start()
+    {
+        player = FindObjectOfType<Controls>();
+    }
+
+    void Update()
+    {
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+            bling.Play();
+            player.crystals++;
+        }
+    }
+}
---
+++ Game2/Assets/Scripts/ObjectMove.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectMove : MonoBehaviour
+{
+
+    public float amounttomovex;
+    public float speed;
+    private float currentposx;
+    private float currentposy;
+    private int facing;
+    
+    
+    void Start()
+    {
+        currentposx = gameObject.transform.position.y;
+        facing = 0;
+        
+
+    }
+
+    
+    void Update()
+    {
+        
+        if (facing == 1 && gameObject.transform.position.y < currentposx - amounttomovex)
+        {
+            facing = 0;
+        }
+
+        if (facing == 0 && gameObject.transform.position.y > currentposx)
+        {
+            facing = 1;
+        }
+
+        
+            if (facing == 0)
+            {
+                transform.Translate(Vector2.up * speed * Time.deltaTime);
+
+            }
+            else if (facing == 1)
+            {
+                transform.Translate(-Vector2.up * speed * Time.deltaTime);
+            }
+        }
+        
+
+    }
